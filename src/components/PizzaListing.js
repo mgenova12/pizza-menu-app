a00ef22bcc220dae6766a9e288fe8424660076ba
Marco <mgenova03@gmail.com>
2019-03-20 13:54:24 -0400

    create app

---
+++ src/components/PizzaListing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class PizzaListing extends Component {
+
+  render() {
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+export default PizzaListing;
---
+++ src/components/PizzaRow.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class PizzaRow extends Component {
+  render() {
+    return (
+      <div>
+      	hi
+      </div>
+    );
+  }
+}
+
+export default PizzaRow;
